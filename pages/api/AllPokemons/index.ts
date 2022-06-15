661fc51f95562f3a2d28dbae44346f604b1057d3
Andres Parada <andress20@gmail.com>
2022-06-14 21:40:27 -0500

    ESLint basic set up

---
+++ pages/api/AllPokemons/index.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import Pokedex from 'pokedex-promise-v2';
+
+
+const getSomething = async (req: NextApiRequest, res: NextApiResponse) => {
+  const P = new Pokedex();
+  const data = await P.getPokemonByName(['eevee', 'ditto']) // with Promise
+    .then((response) => response)
+    .catch((error) => {
+      console.log('There was an ERROR: ', error);
+    });
+  
+
+  res.status(200).json(data);
+};
+
+export default getSomething;
---
+++ pages/src/components/test/test.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+
+const Test: React.FC = () => {
+
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    const data = fetch('api/AllPokemons').then(res => res)
+    setData(data)
+  },[])  
+
+  return (
+    <div>
+            hola
+    </div>
+  )
+}
+export default Test
