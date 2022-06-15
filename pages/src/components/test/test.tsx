a82ad59fdc795430a33de2eaec5cef121e69996b
Andres Parada <andress20@gmail.com>
2022-06-14 22:11:20 -0500

    Prettier basic set up

--- pages/src/components/test/test.tsx
+++ pages/src/components/test/test.tsx
@@ -1,18 +1,13 @@
 import { useEffect, useState } from 'react'
 
 const Test: React.FC = () => {
-
   const [data, setData] = useState({})
 
   useEffect(() => {
-    const data = fetch('api/AllPokemons').then(res => res)
+    const data = fetch('api/AllPokemons').then((res) => res)
     setData(data)
-  },[])  
+  }, [])
 
-  return (
-    <div>
-            hola
-    </div>
-  )
+  return <div>hola</div>
 }
 export default Test
