65937989f68a3bde1465a5229783b98581f9b637
Andres Parada <andress20@gmail.com>
2022-07-28 21:26:14 -0500

    Add themes to the storybook environment

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,18 +1,8 @@
-// import * as NextImage from 'next/image'
-
-// const OriginalNextImage = NextImage.default
-
-// Object.defineProperty(NextImage, 'default', {
-//   configurable: true,
-//   value: props => <OriginalNextImage {...props} unoptimized />,
-// })
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import themes, { Themes } from '../src/themes'
 
 import Image from 'next/image'
 import { muiTheme } from 'storybook-addon-material-ui'
-
-// Image.propTypes = {
-//   unoptimized: null,
-// }
 
 Image.defaultProps = {
   unoptimized: true,
@@ -28,20 +18,50 @@ export const parameters = {
   },
 }
 
-const newTheme = {
-  themeName: 'Grey Theme',
-  palette: {
-    primary1Color: 'red',
-    alternateTextColor: '#4a4a4a',
-    canvasColor: '#616161',
-    textColor: '#bdbdbd',
-    secondaryTextColor: 'rgba(255, 255, 255, 0.54)',
-    disabledColor: '#757575',
-    accent1Color: '#607d8b',
-    primary: {
-      main: '#f00',
+/* Theme Configuration */
+
+//storyThemes contains all themes with an specific themeName that storybook needs to show in Material-UI addon section otherwise only would showing a number
+const storyThemes = [
+  {
+    themeName: 'Light Theme',
+    ...themes[Themes.lightTheme],
+  },
+  {
+    themeName: 'Dark Theme',
+    ...themes[Themes.darkTheme],
+  },
+]
+
+// Add addon on top bar for switching themes
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'lightTheme',
+    toolbar: {
+      icon: 'circlehollow',
+      // Array of plain string values or MenuItem shape (see below)
+      items: ['lightTheme', 'darkTheme'],
+      // Property that specifies if the name of the item will be displayed
+      showName: true,
+      // Change title based on selected value
+      dynamicTitle: true,
     },
   },
 }
 
-export const decorators = [muiTheme([newTheme])]
+const getTheme = themeName => {
+  return themes[themeName]
+}
+
+const withThemeProvider = (Story, context) => {
+  const theme = getTheme(context.globals.theme)
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Story {...context} />
+    </ThemeProvider>
+  )
+}
+
+export const decorators = [withThemeProvider, muiTheme(storyThemes.map(theme => theme))]
--- src/components/organisms/dashboard/index.tsx
+++ src/components/organisms/dashboard/index.tsx
@@ -2,19 +2,19 @@ import { Fragment } from 'react'
 import { PokemonsImages, PokemonUrl } from '@def/IPokemon'
 // TODO - el storybook no esta tomando los @def ts.config!!
 import PokemonCard from '../../molecules/card'
-import { PokemonButton } from '@atoms/button'
+import { PokemonButton } from '../../atoms/button'
 import styles from './dashboard.module.css'
 
 const Dashboard: React.FC<PokemonsImages> = ({ images }) => {
   return (
     <Fragment>
       <h1>Dashboard</h1>
+      <PokemonButton label="clickk" />
       <div className={styles.dashboard}>
         {images.map((image: PokemonUrl) => (
           <PokemonCard key={image.name} title={image.name} image={image.url} />
         ))}
       </div>
-      <PokemonButton />
     </Fragment>
   )
 }
