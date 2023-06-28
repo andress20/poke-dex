de220659fc503ccf6c82883b5db5f6a195ea1f06
Andres Parada <andress20@gmail.com>
2023-06-28 13:47:51 -0500

    refactor[pokemonByScroll]: Change useQuery for useMutation

--- pages/allPokemonsByType/index.tsx
+++ pages/allPokemonsByType/index.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from 'react'
+import { Fragment, useState } from 'react'
 import { useMutation, useQuery } from '@tanstack/react-query'
 import { getPokemonsByType, getAllPokemonTypes } from '@services/services'
 import PokemonTypePicker from '@comp/molecules/typePicker'
@@ -17,38 +17,58 @@ function PokemonsByType() {
     staleTime: 5 * 60 * 1_000,
   })
 
-  const {
-    data: listNamesPokemonsChecked,
-    isLoading: isLoadingListPokemonsChecked,
-    refetch: refetchListNamesChecked,
-  } = useQuery({
-    // eslint-disable-next-line @tanstack/query/exhaustive-deps
-    queryKey: ['PokemonsListNamesByTypes', ...listTypes], // al ponerle aca el listTypes se actualiza apenas doy click en un checkbox sin esperar al boton
-    queryFn: () => getPokemonsByType(data, listTypes),
-    enabled: false,
-    staleTime: 5 * 60 * 1_000,
+  const mutation = useMutation({
+    mutationFn: () => getPokemonsByType(data, listTypes),
+    onSuccess(data) {
+      ;(async function handlerImages() {
+        if (data) {
+          console.log('data', data)
+          const images = await getPokemonsImages([...new Set(data)])
+          setImages(images)
+        }
+      })()
+    },
   })
 
-  // TODO: Try to transform this handler to sumbit format in a form. Maybe is a better practice
-  useEffect(() => {
-    ;(async function handlerImages() {
-      if (listNamesPokemonsChecked) {
-        const images = await getPokemonsImages(listNamesPokemonsChecked)
-        setImages(images)
-      }
-    })()
-  }, [listNamesPokemonsChecked])
+  /**
+   * We can fetch data types with query also like this. But query tryies to fetch when component mount and we don't need that
+   * because first time we haven't choosen anything that cause isLoading keeps true for ever. (if we don't use isLoading all works fine)
+   *
+   * Mutation implementation is better due the first fetch begin when we click button 'apply filter' and we can use isLoading normally
+   */
+
+  // const {
+  //   data: listNamesPokemonsChecked,
+  //   isLoading: isLoadingListPokemonsChecked,
+  //   refetch: refetchListNamesChecked,
+  // } = useQuery({
+  //   // eslint-disable-next-line @tanstack/query/exhaustive-deps
+  //   queryKey: ['PokemonsListNamesByTypes', ...listTypes], // al ponerle aca el listTypes se actualiza apenas doy click en un checkbox sin esperar al boton
+  //   queryFn: () => getPokemonsByType(data, listTypes),
+  //   enabled: false,
+  //   staleTime: 5 * 60 * 1_000,
+  // })
+
+  // // TODO: Try to transform this handler to sumbit format in a form. Maybe is a better practice
+  // useEffect(() => {
+  //   ;(async function handlerImages() {
+  //     if (listNamesPokemonsChecked) {
+  //       const images = await getPokemonsImages(listNamesPokemonsChecked)
+  //       setImages(images)
+  //     }
+  //   })()
+  // }, [listNamesPokemonsChecked])
 
   if (isLoading) return <span>Is Loading List of types...</span>
-
   if (isError) return <span>Something went wrong...</span>
 
   return (
     <Fragment>
       {data && <h1>Types</h1>}
+      {mutation.isLoading && <span> Loading images...</span>}
       <PokemonTypePicker pokemonTypes={transformToPokemonTypeNames(data)} setListTypes={setListTypes} />
-      <button onClick={() => refetchListNamesChecked()}>Apply filter</button>
-      {/* //TODO: el key se esta repitiendo cuando un mismo pokemon esta en varios grupos a la vez como fuego y volador */}
+      {/*<button onClick={() => refetchListNamesChecked()}>Apply filter</button> //query impelementation*/}
+      <button onClick={() => mutation.mutate()}>Apply filter</button>
       <div>
         {images &&
           images.map((image: PokemonUrl) => <PokemonCard key={image.name} title={image.name} image={image.url} />)}
