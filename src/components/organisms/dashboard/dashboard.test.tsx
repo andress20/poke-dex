175520001fb94508f9b8cec1e5ae90ce901c8efa
Andres Parada <andress20@gmail.com>
2023-08-30 21:05:33 -0500

    feat[test-Dashboard]: Add test

---
+++ src/components/organisms/dashboard/dashboard.test.tsx
@@ -0,0 +1,15 @@
+import { renderWithThemeProvider } from '@tests'
+import Dashboard from '@comp/organisms/dashboard'
+
+test('should render dashboard', () => {
+  renderWithThemeProvider(
+    <Dashboard
+      images={[
+        {
+          name: 'charmander',
+          url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg',
+        },
+      ]}
+    />
+  )
+})
