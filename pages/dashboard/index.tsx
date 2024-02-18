d260e7e89021bb3d63c0a94ed865a48e8cb63fed
andress20 <andress20@gmail.com>
2024-02-17 19:09:40 -0500

    comments

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -4,12 +4,12 @@ import { getPokemonsImages } from '@services/services'
 import Dashboard from '@comp/organisms/dashboard'
 import { useQuery } from '@tanstack/react-query'
 import { queryKeys } from '@utils/tanstackQuery/queryKeys'
-import { pokemonsList } from '@pages/login'
+import { pokemonsListDashboard } from '@pages/login'
 
 const DashboardHome: NextPage = () => {
   const { data: pokemonsImages, isLoading: dashboardIsLoading } = useQuery({
-    queryKey: [queryKeys.dashboardPokemons, ...pokemonsList],
-    queryFn: () => getPokemonsImages(pokemonsList),
+    queryKey: [queryKeys.dashboardPokemons, ...pokemonsListDashboard],
+    queryFn: () => getPokemonsImages(pokemonsListDashboard),
     staleTime: 20 * 60 * 1_000, // 20 min
   })
 
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -4,7 +4,7 @@ import type { GetStaticProps, NextPage } from 'next'
 import { QueryClient, dehydrate } from '@tanstack/react-query'
 import { queryKeys } from '@utils/tanstackQuery/queryKeys'
 import { getPokemonsImages } from '@services/services'
-export const pokemonsList = ['charmander', 'squirtle', 'bulbasaur', 'pikachu']
+export const pokemonsListDashboard = ['charmander', 'squirtle', 'bulbasaur', 'pikachu']
 
 /**
  * Passing Message as a children or prop avoid re-render when form changes
@@ -16,14 +16,13 @@ const Message = () => (
 
 const Login: NextPage = () => <LoginForm bottomMessage={<Message />} />
 
+// Prefethind Pokemons of Dashboard and then when Dashboard component load, it will take pokemons from cache
 export const getStaticProps: GetStaticProps = async () => {
-  //const pokemonsList = ['charmander', 'squirtle', 'bulbasaur', 'pikachu']
-
   const queryClient = new QueryClient()
 
   await queryClient.prefetchQuery({
-    queryKey: [queryKeys.dashboardPokemons, ...pokemonsList],
-    queryFn: () => getPokemonsImages(pokemonsList),
+    queryKey: [queryKeys.dashboardPokemons, ...pokemonsListDashboard],
+    queryFn: () => getPokemonsImages(pokemonsListDashboard),
   })
 
   return { props: { dehydratedState: dehydrate(queryClient) } }
