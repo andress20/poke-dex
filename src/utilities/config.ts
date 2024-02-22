62b313153d786c142d0f5f087288f3124a5a09a9
Andres Parada <andress20@gmail.com>
2024-02-22 11:44:00 -0500

    remove unnecesarry enviroment variable DEFAULT-IMAGE

--- src/utilities/config.ts
+++ src/utilities/config.ts
@@ -2,7 +2,6 @@ import { IConfig } from '@src/types/IConfig'
 
 export const config: IConfig = {
   siteUrl: process.env.NEXT_PUBLIC_SM_URL,
-  defaultImage: process.env.DEFAULT_IMAGE,
   pokemonApi: process.env.POKEMON_API_URL,
   pokemonTypesApi: process.env.POKEMON_TYPES_API_URL,
   apiLimitUrlParameter: process.env.API_LIMIT_URL_PARAMETER ? parseInt(process.env.API_LIMIT_URL_PARAMETER) : 12,
